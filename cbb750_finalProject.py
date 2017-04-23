cba394b51b5ce8cc32cfe1c39385534b2853742e
Kevin <kevin.lopez.91@gmail.com>
2017-04-23 08:36:19 -0400

    Added multithreading

--- cbb750_finalProject.py
+++ cbb750_finalProject.py
@@ -9,15 +9,17 @@ Created on Thu Apr 13 10:08:24 2017
 import urllib, json
 import pymongo
 '''Get the records from fda'''
-def getRecords(num_records = 100):
+def getRecords(num_records = 100, start_date='20120101', end_date='20161231'):
     #num_records = 4587015
     records = []
     i = 0
     while i in range(0,num_records):#go through all records
         i+=100
-        url = "https://api.fda.gov/drug/event.json?search=receivedate:[20120101+TO+20161231]&limit=100" + "&skip=" + str(i)
+        #i = i%5000#limit for skip, as per FDA api
+        url = "https://api.fda.gov/drug/event.json?api_key=QIYs3xaRHyjYEkcprTinxbS6QXABp4cu55ZcR2iV&search=receivedate:[" + str(start_date) + "+TO+" + str(end_date) + "]&limit=100" + "&skip=" + str(i)
         response = urllib.urlopen(url)
         data = json.loads(response.read())
+        print i
         results = data["results"]
         records.extend(results)
     '''
@@ -244,8 +246,14 @@ def random_forest(x_train, y_train, estimators = 100, maxDepth = None, randomSta
     model.fit(x_train, y_train)
     return model
 
-
-
+'''
+split arrays into evenly sized chunks
+'''
+def chunks(array, n):
+    """Yield successive n-sized chunks from array."""
+    for i in xrange(0, len(array), n):
+        yield array[i:i + n]
+        
 #Auto encodes any dataframe column of type category or object.
 def dummyEncode(df):
     from sklearn.preprocessing import LabelEncoder
@@ -258,31 +266,59 @@ def dummyEncode(df):
             print('Error encoding '+feature)
     return df
 
+def one_hot_encode(categorical_labels):
+    res = []
+    tmp = None
+    for col in categorical_labels:
+        v = x[col].astype(str).str.strip('[]').str.get_dummies(', ')#cant set a prefix
+        if len(res) == 2:
+            tmp = pandas.concat(res, axis=1)
+            del res
+            res = []
+            res.append(tmp)
+            del tmp
+            tmp = None
+        else:
+            res.append(v)
+    result = pandas.concat(res, axis=1)
+    return result
+
 if __name__ == '__main__':
     #main method
     records = getRecords(num_records=1000)#get records
     
+    #save to disk
+    with open('data.json', 'w') as outfile:
+        json.dump(records, outfile)
+    #read from disk
+    with open('data.json') as json_data:
+        records = json.load(json_data)
     subrecords = []#Get all the records and pull out the stuff we need
     for record in records:
         subrecords.append(getNessessaryKeyValues(record))
+    del records#save memory
+    del record
     
     import pandas
-    #x_y_dataframe = pandas.DataFrame(subrecords,index=[range(0,len(subrecords))])#put into a dataframe
     x_y_dataframe = pandas.DataFrame(subrecords)
-    x_y_dataframe2 = x_y_dataframe.dropna(axis=0)
+    #x_y_dataframe.dropna(how='any')#here we can drop a row if there is any na
+    del subrecords
+    
+    #x_y_dataframe2 = x_y_dataframe.dropna(axis=0)
     #get labels
     (x,y) = get_y_vals(x_y_dataframe, 'serious')
+    del x_y_dataframe
     #df = pandas.DataFrame([{'drug': ['drugA','drugB'], 'patient': 'john'}, {'drug': ['drugC','drugD'], 'patient': 'angel'}])
     
-    res = []
     categorical_labels = [
+ 'actiondrug',
  'brand_name',
  'companynumb',
  'drugadministrationroute',
  'drugcharacterization',
  'drugdosageform',
  'drugdosagetext',
- #'drugenddate',
+ 'drugenddate',
  'drugindication',
  'drugstartdate',
  #'duplicate',
@@ -291,7 +327,7 @@ if __name__ == '__main__':
  'generic_name',
  'manufacturer_name',
  'medicinalproduct',
- #'nui',
+ 'nui',#has lists
  'occurcountry',
  'package_ndc',
  'patientdeath',
@@ -321,12 +357,38 @@ if __name__ == '__main__':
  'substance_name',
  #'transmissiondate'
  ]
-    for col in categorical_labels:
-        v = x[col].astype(str).str.strip('[]').str.get_dummies(', ')
-        res.append(v)
+    #save data
+    x.to_pickle('x_data.pkl')
+    y.to_pickle('y_data.pkl')
+    #load data
+    import pandas
+    x = pandas.read_pickle('x_data.pkl')
+    y = pandas.read_pickle('y_data.pkl')
+    
+    '''
+    get difference in labeles we are one-hot encoding and ones we aren't
+    We need to preserve the ones we arent one-hot encoding
+    '''
+    s = set(categorical_labels)
+    not_categorical_labels = [x_val for x_val in list(x) if x_val not in s]
+    x_non_cat_labels = x[not_categorical_labels]#save non cat to temp var
+    
+    num_processors = 4
+    n_size = int(round(len(categorical_labels)/num_processors))
+    import time
+    import multiprocessing
+    
+    start_time = time.time()
+    p = multiprocessing.Pool(num_processors)
+    res = (p.map(one_hot_encode, list(chunks(categorical_labels, n_size))))
+    print (time.time()-start_time)
+    p.terminate()#kill the workers
+    res.append(x_non_cat_labels)
+    del x_non_cat_labels
     x = pandas.concat(res, axis=1)
     y = pandas.to_numeric(y.astype(str).str.strip('[]'))
-    
+    x = x.dropna(how='any')
+    y = y[x.index]
 #    y[y.isnull()] = 0#we do this to convert nans, if its empty, we set it to 0
     #y[0:20] = 1#just a test
 #    import numpy as np
@@ -335,8 +397,9 @@ if __name__ == '__main__':
     #split the data:
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+    del x, y
     #do a logistic regression
-    model = logistic_regression(X_train, list(y_train), boosting=False)
+    model = logistic_regression(X_train, list(y_train), bagging=True)
     y_hat = model.predict(X_test)#predict
     y_test = y_test.reset_index(drop=True)    
     from sklearn.metrics import accuracy_score
