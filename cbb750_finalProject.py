c58437f83c5c4922ed90118244ae0ca45bb2fb96
Kevin <kevin.lopez.91@gmail.com>
2017-04-23 05:22:21 -0400

    Added one-hot encoding to allow for model training

--- cbb750_finalProject.py
+++ cbb750_finalProject.py
@@ -55,58 +55,100 @@ def getNessessaryKeyValues(record):
             'occurcountry',
             'primarysourcecountry',
             #'primarysource',
-            'primarysource.qualification',
-            'primarysource.reportercountry',
+            'qualification',
+            'reportercountry',
             #'reportduplicate',
-            'reportduplicate.duplicatesource',
-            'reportduplicate.duplicatenumb',
-            "patient.patientonsetage",
-            "patient.patientonsetageunit",
-            "patient.patientsex",
-            "patient.patientweight",
-            "patient.patientdeath",
-            "patient.patientdeath.patientdeathdate",
-            "patient.drug.actiondrug",
-            "patient.drug.drugadditional",
-            "patient.drug.drugcumulativedosagenumb",
-            "patient.drug.drugcumulativedosageunit",
-            "patient.drug.drugdosageform",
-            "patient.drug.drugintervaldosagedefinition",
-            "patient.drug.drugrecurreadministration",
-            "patient.drug.drugseparatedosagenumb",
-            "patient.drug.drugadministrationroute",
-            "patient.drug.drugcharacterization",
-            "patient.drug.drugdosagetext",
-            'patient.drug.drugenddate',
-            'patient.drug.drugindication',
-            'patient.drug.drugstartdate',
-            'patient.drug.drugtreatmentduration',
-            'patient.drug.drugtreatmentdurationunit',
-            'patient.drug.medicinalproduct',
-            'patient.drug.brand_name',
-            'patient.drug.generic_name',
-            'patient.drug.manufacturer_name',
-            'patient.drug.nui',
-            'patient.drug.package_ndc',
-            'patient.drug.pharm_class_cs',
-            'patient.drug.pharm_class_epc',
-            'patient.drug.pharm_class_pe',
-            'patient.drug.pharm_class_moa',
-            'patient.drug.product_ndc',
-            'patient.drug.rxcui',
-            'patient.drug.substance_name',
-            'patient.reaction.reactionmeddrapt',
-            'patient.reaction.reactionmeddraversionpt',
-            'patient.reaction.reactionoutcome'
+            'duplicatesource',
+            'duplicatenumb',
+            "patientonsetage",
+            "patientonsetageunit",
+            "patientsex",
+            #"patient.patientweight",#interseting, this data may be useful but is not recorded much
+            "patientdeath",
+            "patientdeathdate",
+            "actiondrug",
+            #"patient.drug.drugadditional",
+            #"patient.drug.drugcumulativedosagenumb",
+            #"patient.drug.drugcumulativedosageunit",
+            "drugdosageform",
+            #"patient.drug.drugintervaldosagedefinition",
+            #"patient.drug.drugrecurreadministration",
+            #"patient.drug.drugseparatedosagenumb",
+            "drugadministrationroute",
+            "drugcharacterization",
+            "drugdosagetext",
+            'drugenddate',#sometimes patients do not have an end date? maybe replace with some value like -1
+            'drugindication',
+            'drugstartdate',
+            #'patient.drug.drugtreatmentduration',
+            #'patient.drug.drugtreatmentdurationunit',
+            'medicinalproduct',
+            'brand_name',
+            'generic_name',
+            'manufacturer_name',
+            'nui',
+            'package_ndc',
+            'pharm_class_cs',
+            'pharm_class_epc',
+            'pharm_class_pe',
+            'pharm_class_moa',
+            'product_ndc',
+            'rxcui',
+            'substance_name',
+            'reactionmeddrapt',
+            'reactionmeddraversionpt',
+            'reactionoutcome'
             ]
     subrecord = {}
     for f in fields:
-        extracted_record = get_dotted(record, f)
+        extracted_record = get_recursively(record, f)
         #we get the key and try to convert to float, otherwise return the same val
-        extracted_record[extracted_record.keys()[0]] = convert_to_float(extracted_record.values()[0])
-        subrecord.update(extracted_record)
+        s_record = {}
+        if not extracted_record:
+            s_record[f] = None
+        elif(len(extracted_record) < 2):#if the result is just 1 value, then return that value not a list
+            s_record[f] = extracted_record[0]
+        else:
+            s_record[f] = extracted_record
+        subrecord.update(s_record)
     return subrecord
 
+
+
+def get_recursively(search_dict, field):
+    """Takes a dict with nested lists and dicts,
+    and searches all dicts for a key of the field
+    provided.
+    """
+    fields_found = []
+
+    for key, value in search_dict.iteritems():
+
+        if key == field:
+            fields_found.append(value)
+
+        elif isinstance(value, dict):
+            results = get_recursively(value, field)
+            for result in results:
+                fields_found.append(result)
+
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    more_results = get_recursively(item, field)
+                    for another_result in more_results:
+                        fields_found.append(another_result)
+    #we want to return a list of values, and we do not want to try and iterate over some none type
+    
+    new_field_found = []
+    if type(fields_found) == list:
+        for sublist in fields_found:
+            if type(sublist) == list:
+                for val in sublist:
+                    new_field_found.append(convert_to_float(val))#also convert to float
+            else:
+                new_field_found.append(convert_to_float(sublist))#also convert to float
+    return list(set(new_field_found))
 
 '''
 This gives us the keys using the dot notation. It also gives us an empty 
@@ -126,14 +168,14 @@ def get_dotted(d, keys):
             d = d[0]
         if type(d[key]) != list and type(d[key]) != dict:#make sure its either a dict or list, otherwise return empty
             if keys not in d:
-                d[keys] = ""#some empty value
+                d[keys] = None#some empty value
             return {rest: d[key]}
         return get_dotted(d[key], rest)
     else:
         if type(d) == list:#make sure its not a list
             d = d[0]
         if keys not in d:
-            d[keys] = ""#some empty value
+            d[keys] = None#some empty value
         
         subDic = {keys: d[keys]}
         return subDic
@@ -148,7 +190,7 @@ def convert_to_float(s):
 
 def get_y_vals(df, y_label):
     #df.loc[pandas.isnull(df[y_label])] = ""#missing data
-    df.loc[df[y_label].str.contains("", na=False)] = 0
+    #df.loc[df[y_label].str.contains("", na=False)] = 0
     y = df[y_label]
     del df[y_label]
     return (df, y)#return the new dataframe and y array
@@ -202,6 +244,20 @@ def random_forest(x_train, y_train, estimators = 100, maxDepth = None, randomSta
     model.fit(x_train, y_train)
     return model
 
+
+
+#Auto encodes any dataframe column of type category or object.
+def dummyEncode(df):
+    from sklearn.preprocessing import LabelEncoder
+    columnsToEncode = list(df.select_dtypes(include=['category','object']))
+    le = LabelEncoder()
+    for feature in columnsToEncode:
+        try:
+            df[feature] = le.fit_transform(df[feature])
+        except:
+            print('Error encoding '+feature)
+    return df
+
 if __name__ == '__main__':
     #main method
     records = getRecords(num_records=1000)#get records
@@ -211,32 +267,95 @@ if __name__ == '__main__':
         subrecords.append(getNessessaryKeyValues(record))
     
     import pandas
-    x_y_dataframe = pandas.DataFrame(subrecords,index=[range(0,len(subrecords))])#put into a dataframe
+    #x_y_dataframe = pandas.DataFrame(subrecords,index=[range(0,len(subrecords))])#put into a dataframe
+    x_y_dataframe = pandas.DataFrame(subrecords)
+    x_y_dataframe2 = x_y_dataframe.dropna(axis=0)
     #get labels
-    (x,y) = get_y_vals(x_y_dataframe, 'seriousnessdeath')
+    (x,y) = get_y_vals(x_y_dataframe, 'serious')
+    #df = pandas.DataFrame([{'drug': ['drugA','drugB'], 'patient': 'john'}, {'drug': ['drugC','drugD'], 'patient': 'angel'}])
+    
+    res = []
+    categorical_labels = [
+ 'brand_name',
+ 'companynumb',
+ 'drugadministrationroute',
+ 'drugcharacterization',
+ 'drugdosageform',
+ 'drugdosagetext',
+ #'drugenddate',
+ 'drugindication',
+ 'drugstartdate',
+ #'duplicate',
+ 'duplicatenumb',
+ 'duplicatesource',
+ 'generic_name',
+ 'manufacturer_name',
+ 'medicinalproduct',
+ #'nui',
+ 'occurcountry',
+ 'package_ndc',
+ 'patientdeath',
+ 'patientdeathdate',
+ #'patientonsetage',
+ 'patientonsetageunit',
+ 'patientsex',
+ 'pharm_class_cs',
+ 'pharm_class_epc',
+ 'pharm_class_moa',
+ 'pharm_class_pe',
+ 'primarysourcecountry',
+ 'product_ndc',
+ #'qualification',
+ 'reactionmeddrapt',
+ 'reactionmeddraversionpt',
+ 'reactionoutcome',
+ #'receivedate',
+ 'reportercountry',
+ 'rxcui',
+ #'safetyreportid',
+ 'seriousnessdeath',
+ 'seriousnessdisabling',
+ 'seriousnesshospitalization',
+ 'seriousnesslifethreatening',
+ 'seriousnessother',
+ 'substance_name',
+ #'transmissiondate'
+ ]
+    for col in categorical_labels:
+        v = x[col].astype(str).str.strip('[]').str.get_dummies(', ')
+        res.append(v)
+    x = pandas.concat(res, axis=1)
+    y = pandas.to_numeric(y.astype(str).str.strip('[]'))
+    
+#    y[y.isnull()] = 0#we do this to convert nans, if its empty, we set it to 0
     #y[0:20] = 1#just a test
+#    import numpy as np
+#    x[np.isfinite(x['drugintervaldosagedefinition'])]
+    #x_y_dataframe.dropna(how='any')#here we can drop a row if there is any na
     #split the data:
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-    
     #do a logistic regression
     model = logistic_regression(X_train, list(y_train), boosting=False)
     y_hat = model.predict(X_test)#predict
+    y_test = y_test.reset_index(drop=True)    
+    from sklearn.metrics import accuracy_score
+    accuracy_score(y_test, y_hat)
     
-    '''
-    TODO:
-        Need to figure out why when I try to fit the model it cant convert string to float?
-        
-    '''
-    X_train.apply(lambda x: pandas.factorize(x)[0])#something is wrong when trying to do many rows, probably due to empty or null or something.
+    
     #do a random forest
     model = random_forest(X_train, list(y_train))
     y_hat = model.predict(X_test)#predict
+    y_test = y_test.reset_index(drop=True)    
+    from sklearn.metrics import accuracy_score
+    accuracy_score(y_test, y_hat)
     
     #do a naive bays
     model = naive_bays(X_train, list(y_train), bagging=True)
     y_hat = model.predict(X_test)#predict
-    
+    y_test = y_test.reset_index(drop=True)    
+    from sklearn.metrics import accuracy_score
+    accuracy_score(y_test, y_hat)    
     
     
     collection = startmongodb()#get collection
